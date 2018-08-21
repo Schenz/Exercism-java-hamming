b17e18b40f131f08d1ab837fb93a4ea731cce2e0
Schenz <bschenz@gmail.com>
2018-08-21 10:35:36 -0400

    FINAL SOLUTION

--- src/main/java/Hamming.java
+++ src/main/java/Hamming.java
@@ -1,11 +1,27 @@
 class Hamming {
 
+    private String leftStrand;
+    private String rightStrand;
+
     Hamming(String leftStrand, String rightStrand) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        if (leftStrand.length() != rightStrand.length()) {
+            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+        }
+
+        this.leftStrand = leftStrand;
+        this.rightStrand = rightStrand;
     }
 
     int getHammingDistance() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        int distance = 0;
+
+        for (int index1 = 0; index1 < this.leftStrand.length(); index1++) {
+            if (this.leftStrand.charAt(index1) != this.rightStrand.charAt(index1)) {
+                distance++;
+            }
+        }
+
+        return distance;
     }
 
 }
